f3cf784720c459ce3b5a6c9fe764f23e16e4a41e
DEMONCRXSS <156143511+DEMONCRXSS@users.noreply.github.com>
2025-05-19 20:39:49 -0600

    19-06

---
+++ Assets/Scrips/DanceController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DanceController : MonoBehaviour
+{
+    [SerializeField]
+
+    private UnityEvent _onActivateSelectDance;
+    [SerializeField]
+
+    private UnityEvent _onSelectDance;
+    public void ActivateSelectDance()
+    {
+        _onActivateSelectDance?.Invoke();
+    }
+
+    public void SelectDance()
+    {
+        _onActivateSelectDance?.Invoke();
+    }
+}
